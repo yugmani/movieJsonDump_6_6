0b7bced648308a398e6dde4a3bd39ae0bcbd846d
Himalaya <yugmani@yahoo.com>
2020-08-11 01:34:36 -0700

    DONE: all required functionalities

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 
 ## File
 
-* [`movie-json-dump`](Unsolved/movie-json-dump.html)
+* [`movie-json-dump`](index.html)
 
 ## Instructions
 
-* Using `movie-json-dump.html` in `06-MovieJSONDump` as starter code, add functionality such that clicking `Movie Search` triggers an AJAX call to the OMDb database and the JSON response to be appended onto the page.
+* Using `index.html` as starter code, add functionality such that clicking `Movie Search` triggers an AJAX call to the OMDb database and the JSON response to be appended onto the page.
 
 * **HINT:** You will need to use [JSON.stringify()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify) to render the AJAX response as text. 
 
---
+++ script.js
@@ -0,0 +1,78 @@
+const movies = [];
+        
+// This .on("click") function will trigger the AJAX Call
+$("#find-movie").on("click", function(event) {
+
+  // Preventing the submit button from trying to submit the form
+  // We're optionally using a form so the user may hit Enter to search instead of clicking the button
+  event.preventDefault();
+
+  
+  // Here we grab the text from the input box
+  var movie = $("#movie-input").val();
+
+  
+  var newSearch = document.createElement("h5");
+  newSearch.textContent = movie;
+  $(".search-list").append(newSearch);
+ 
+ 
+
+  // Here we construct our URL
+  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+  // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
+  // and display it in the div with an id of movie-view
+
+  // YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE
+
+  // =================================================================
+
+  // CODE GOES HERE
+  $.ajax({
+      url:queryURL,
+      method: "GET"
+  }).then(function(response){
+      
+      // console.log(response.Runtime);
+      // $("#movie-view").text(JSON.stringify(response));
+     
+      displayMovie(response);
+  });
+
+
+  const displayMovie = (data)=>{
+      const actors = data.Actors.split(",");
+      
+      let html = `
+      <div class="card" style="width: 22rem;">
+          <div class="card-body">
+          
+          <img src=${data.Poster} class="card-img-top" alt="movie poster">
+          <p class="card-text">${data.Plot}</p>
+      </div>`
+      $("#movie-view").html(html);
+
+      let ht = `
+      <h1 class="card-title text-uppercase">${data.Title}</h1>
+      <ul class="list-group list-group-flush">
+      <li class="list-group-item"><span class="font-weight-bold">Director: </span>${data.Director}</li>
+      <li class="list-group-item"><span class="font-weight-bold">Writer: </span>${data.Writer}</li>
+      <li class="list-group-item"><span class="font-weight-bold">Genre: </span>${data.Genre}</li>
+      <li class="list-group-item"><span class="font-weight-bold">Awards: </span>${data.Awards}</li>
+      
+      <li class="list-group-item"><span class="font-weight-bold">Rated: </span>${data.Rated}</li>
+      <li class="list-group-item"><span class="font-weight-bold">Released: </span>${data.Released}</li>
+      <h5 class="card-title">Actors</h5>`
+      
+     actors.forEach(element => {
+          ht += `<li class="list-group-item">${element}</li>`
+     });
+
+      ht += ` <ul> `  ;
+      
+      $("#movie-info").html(ht);
+
+  }
+
+});
